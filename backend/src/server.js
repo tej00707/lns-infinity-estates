9e92a71dd146f56772714d85e20784658f707a3d
Tejovanth <185246997+tej00707@users.noreply.github.com.>
2026-06-19 11:32:05 +0530

    Fix CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://lnsinfinityestates.vercel.app/'
+    'https://lnsinfinityestates.vercel.app/api'
   ],
   credentials: true
 }));
